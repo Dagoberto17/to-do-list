9ff80896bed5a4cc59b50b5961bfb38f9f6f4911
Dagoberto del Rio Barba <115412194+Dagoberto17@users.noreply.github.com>
2026-04-04 14:48:02 -0600

    Agregar archivo dato.js con clase Nota
    Se creó el archivo dato.js (antes agregar.js) para manejar la lógica de datos.
    Incluye la clase Nota para la creación de nuevas tareas en la lista.

--- js/dato.js
+++ js/dato.js
@@ -1,1 +1,27 @@
+// Proyecto: To Do List
+// Autor: Dagoberto17
+// Descripción: Función de agregar nuevas notas
+// Fecha: Marzo 2026
+
+// ============ Clase para agregar ==============
+
+class Nota {
+    static contadorNotas = 0; //Sirve para contabilizar el número de notas (indexar)
+    constructor(nuevaNota) {
+        this._nuevaNota = nuevaNota;
+        this._id = ++Nota.contadorNotas;
+    }
+    //Método get para visualizar las notas y el id
+    get nuevaNota() {
+        return this._nuevaNota;
+    }
+    get id() {
+        return this._id;
+    }
+    //Método set para editar las notas
+    set nuevaNota(nuevaNota) {
+        this._nuevaNota = nuevaNota;
+    }
+};
+
 
