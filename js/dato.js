d1c5855759c33464a38636037f1e6902771a93fb
Dagoberto del Rio Barba <115412194+Dagoberto17@users.noreply.github.com>
2026-04-08 17:13:54 -0600

    Renombrar parámetro de Nota a 'descripción'
    - Se actualizó la clase Nota para usar el parámetro 'descripción' en lugar de 'nuevaNota'.
    - Se ajustaron los métodos getter y setter para reflejar el nuevo nombre.

--- js/dato.js
+++ js/dato.js
@@ -6,22 +6,20 @@
 // ============ Clase para agregar ==============
 
 class Nota {
-    static contadorNotas = 0; //Sirve para contabilizar el número de notas (indexar)
-    constructor(nuevaNota) {
-        this._nuevaNota = nuevaNota;
+    static contadorNotas = 0;
+    constructor(descripción) {
+        this._descripción = descripción;
         this._id = ++Nota.contadorNotas;
     }
-    //Método get para visualizar las notas y el id
-    get nuevaNota() {
-        return this._nuevaNota;
+    get descripción() {
+        return this._descripción;
     }
     get id() {
         return this._id;
     }
-    //Método set para editar las notas
-    set nuevaNota(nuevaNota) {
-        this._nuevaNota = nuevaNota;
+    set descripción(descripción) {
+        this._descripción = descripción;
     }
-};
+}
 
 
