f003666a5d8c34443ee70a90461e37b67b500f87
Dagoberto del Rio Barba <115412194+Dagoberto17@users.noreply.github.com>
2026-04-27 12:58:07 -0600

    Se agrego el atributo `estado` a la clase.
    - Se agrego el atributo `estado` inicializado en "pendiente",
    - Se agrego método set y get para este atributo.

--- js/dato.js
+++ js/dato.js
@@ -10,6 +10,7 @@ class Nota {
     constructor(descripción) {
         this._descripción = descripción;
         this._id = ++Nota.contadorNotas;
+        this._estado = "pendiente";
     }
     get descripción() {
         return this._descripción;
@@ -20,6 +21,13 @@ class Nota {
     set descripción(descripción) {
         this._descripción = descripción;
     }
+    get estado() {
+        return this._estado;
+    }
+    set estado(estado) {
+        this._estado = estado;
+    }
 }
+
 
 
